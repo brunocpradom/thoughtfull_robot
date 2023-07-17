8919e6d945c3ed023b68e2b1360250b217f5687e
brunocpradom <brunocpradom@gmail.com>
2023-07-17 16:20:37 -0300

    fix: logger import

--- tasks.py
+++ tasks.py
@@ -3,8 +3,7 @@ from __future__ import annotations
 import time
 from dataclasses import asdict
 
-from loguru import logger
-
+from loguru import logger as loguru_logger
 from robocorp.tasks import task
 
 from src.ScrappingNews.config import get_environment_variables
@@ -14,7 +13,7 @@ from src.ScrappingNews.services.ny_times import NYTimesScrapperService
 @task
 def task():
     config = get_environment_variables()
-    logger = logger
+    logger = loguru_logger
     logger.add("logs/logs.log", retention="5 days")
 
     robot = NYTimesScrapperService(config, logger)
